37d4987efe3c65a9fb38aed9cbc94302e5767787
Samuel Brasileiro <samuelbsantosn@gmail.com>
2020-03-31 12:36:10 -0300

    Add orange tab with writeable logo

--- cocreate.playground/Contents.swift
+++ cocreate.playground/Contents.swift
@@ -4,6 +4,47 @@ import UIKit
 import PlaygroundSupport
 
 // Present the view controller in the Live View window
+let cfURL = Bundle.main.url(forResource: "Gilbert-Color", withExtension: "otf")! as CFURL
+CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
+
 let vc = MyViewController()
+
+public class MyViewController : UIViewController {
+    override public func loadView() {
+        self.view = UIView()
+        view.backgroundColor = .white
+//        let line = UIView(frame: CGRect(x: 400, y: 0, width: 1, height: 600))
+//        line.backgroundColor = .lightGray
+//        view.addSubview(line)
+        
+        let orangeTab = UIView(frame: CGRect(x: 0, y: 0, width: 800, height: 88))
+        orangeTab.backgroundColor = .myOrange
+        
+        let logoBackground = UILabel(frame: CGRect(x: 280, y: 15, width: 240, height: 60))
+        logoBackground.backgroundColor = .myLightGrey
+        logoBackground.layer.masksToBounds = true
+        logoBackground.layer.cornerRadius = 25
+        
+        let label = UILabel(frame: CGRect(x: 0, y: -10, width: 240, height: 60))
+        label.font = UIFont(name: "Gilbert Color", size: 80)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.text = "co-create"
+        
+        
+        
+        view.addSubview(orangeTab)
+            orangeTab.addSubview(logoBackground)
+                logoBackground.addSubview(label)
+    }
+    
+    override public func viewDidLoad(){
+        
+    }
+}
+
 vc.preferredContentSize = CGSize(width: 800, height: 600)
+
+
+
 PlaygroundPage.current.liveView = vc
---
+++ cocreate.playground/Sources/Colors.swift
@@ -0,0 +1,7 @@
+import Foundation
+import UIKit
+
+public extension UIColor {
+    static let myOrange = UIColor(red: 227.0/255.0, green: 119.0/255.0, blue: 56.0/255.0, alpha: 1.0)
+    static let myLightGrey = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+}
