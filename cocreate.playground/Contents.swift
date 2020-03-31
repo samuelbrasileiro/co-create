303086301d4d3e7d75df534563c2290d79566fc7
Samuel Brasileiro <samuelbsantosn@gmail.com>
2020-03-31 17:23:07 -0300

    Using CircleProgressView to match levels

--- cocreate.playground/Contents.swift
+++ cocreate.playground/Contents.swift
@@ -2,6 +2,7 @@
   
 import UIKit
 import PlaygroundSupport
+import MapKit
 
 // Present the view controller in the Live View window
 let cfURL = Bundle.main.url(forResource: "Gilbert-Color", withExtension: "otf")! as CFURL
@@ -9,7 +10,20 @@ CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
 
 let vc = MyViewController()
 
-public class MyViewController : UIViewController {
+public class MyViewController : UIViewController,  MKMapViewDelegate{
+    
+    let mapView = MKMapView(frame: CGRect(x:0, y:88, width: 800, height:424))
+
+    var mapRegion = MKCoordinateRegion()
+    let menuIcon = UIButton()
+    
+    let cityLabel = UILabel()
+    
+    let progressBar = CircleProgressView()
+    
+    let progressName = UILabel()
+    
+    let achievementButton = UIButton()
     override public func loadView() {
         self.view = UIView()
         view.backgroundColor = .white
@@ -23,7 +37,7 @@ public class MyViewController : UIViewController {
         let logoBackground = UILabel(frame: CGRect(x: 280, y: 15, width: 240, height: 60))
         logoBackground.backgroundColor = .myLightGrey
         logoBackground.layer.masksToBounds = true
-        logoBackground.layer.cornerRadius = 25
+        logoBackground.layer.cornerRadius = 17
         
         let label = UILabel(frame: CGRect(x: 0, y: -10, width: 240, height: 60))
         label.font = UIFont(name: "Gilbert Color", size: 80)
@@ -33,9 +47,69 @@ public class MyViewController : UIViewController {
         
         
         
+        menuIcon.contentMode = .scaleToFill
+        menuIcon.frame = CGRect(x: 20, y: 20, width: 54, height: 42)
+        menuIcon.setImage(UIImage(imageLiteralResourceName: "icons-menu.png"), for: .normal)
+        
+        let belowTab = UIView(frame: CGRect(x: 0, y: 512, width: 800, height: 88))
+        belowTab.backgroundColor = .myLightGrey
+        let line = UIView(frame: CGRect(x: 0, y: 0, width: 800, height: 1))
+        line.backgroundColor = .lightGray
+        
+        mapView.delegate = self
+        
+        
+        cityLabel.frame = CGRect(x: belowTab.frame.width/2 - 140, y: 10, width: 280, height: 60)
+        cityLabel.font = UIFont(name: "Gilbert Color", size: 60)
+        cityLabel.textAlignment = .center
+        cityLabel.adjustsFontSizeToFitWidth = true
+        
+        cityLabel.text = "Recife"
+        
+        progressBar.frame = CGRect(x: 15, y: 15, width: 58, height: 58)
+        progressBar.centerFillColor = .clear
+        progressBar.backgroundColor = .clear
+        progressBar.clockwise = true
+        progressBar.setProgress(0.9, animated: true)
+        progressBar.trackBackgroundColor = .clear
+        progressBar.trackWidth = 8
+        progressBar.trackFillColor = .myLightBlue
+        
+        let number = UILabel(frame: CGRect(x: 10, y: 10, width: 38, height: 40))
+        number.font = UIFont(name: "Gilbert Color", size: 37)
+        number.textAlignment = .center
+        number.adjustsFontSizeToFitWidth = true
+        number.text = String(12)
+        
+        achievementButton
+        
+        
+        let recifeCoordinates = CLLocationCoordinate2DMake(-8.0522, -34.9286)
+
+        let mapRegionSpan = 0.02
+        mapRegion.center = recifeCoordinates
+        mapRegion.span.latitudeDelta = mapRegionSpan
+        mapRegion.span.longitudeDelta = mapRegionSpan
+
+        mapView.setRegion(mapRegion, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = recifeCoordinates
+        annotation.title = "Hellcife"
+        annotation.subtitle = "O centro de Recife é aqui"
+        
+        
+        mapView.addAnnotation(annotation)
         view.addSubview(orangeTab)
             orangeTab.addSubview(logoBackground)
                 logoBackground.addSubview(label)
+            orangeTab.addSubview(menuIcon)
+        view.addSubview(mapView)
+        view.addSubview(belowTab)
+            belowTab.addSubview(line)
+            belowTab.addSubview(cityLabel)
+            belowTab.addSubview(progressBar)
+                progressBar.addSubview(number)
     }
     
     override public func viewDidLoad(){
--- cocreate.playground/Sources/Colors.swift
+++ cocreate.playground/Sources/Colors.swift
@@ -4,4 +4,5 @@ import UIKit
 public extension UIColor {
     static let myOrange = UIColor(red: 227.0/255.0, green: 119.0/255.0, blue: 56.0/255.0, alpha: 1.0)
     static let myLightGrey = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+    static let myLightBlue = UIColor(red: 78.0/255.0, green: 177.0/255.0, blue: 222.0/255.0, alpha: 1.0)
 }
