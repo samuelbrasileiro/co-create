7d28c9b05da80c8bf04151c77c9fe7d94a159d2b
Samuel Brasileiro <samuelbsantosn@gmail.com>
2020-04-04 21:00:48 -0300

    Adicionando Collection View e header - OBS.: Falta resolver problema das reusable views

--- cocreate.playground/Contents.swift
+++ cocreate.playground/Contents.swift
@@ -6,14 +6,87 @@ import MapKit
 import CoreLocation
 import XCPlayground
 // Present the view controller in the Live View window
-let cfURL = Bundle.main.url(forResource: "Gilbert-Color", withExtension: "otf")! as CFURL
+var cfURL = Bundle.main.url(forResource: "Gilbert-Color", withExtension: "otf")! as CFURL
+CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
+cfURL = Bundle.main.url(forResource: "Gilbert-Bold", withExtension: "otf")! as CFURL
 CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
 
+let bank = AchievementBank()
 
 
-public extension AchievementsViewController{
+class AchievementCollectionViewCell: UICollectionViewCell{
+    let imageView = UIImageView()
+    let frontView = UIView()
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .myLightGrey
+        layer.masksToBounds = true
+        layer.cornerRadius = 18
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        frontView.frame = imageView.frame
+        frontView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+        frontView.isHidden = true
+        imageView.contentMode = .scaleAspectFit
+        self.addSubview(imageView)
+        self.addSubview(frontView)
+    }
+    
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class SupView: UICollectionReusableView {
 
-    override func loadView() {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.myCustomInit()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.myCustomInit()
+    }
+
+    func myCustomInit() {
+        let label = UILabel(frame: CGRect(x: (824)/2 - 273/2, y: 20, width: 273, height: 80))
+        label.font = UIFont(name: "Gilbert", size: 80)
+        label.textColor = .myLightGrey
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.text = "Conquistas"
+        self.addSubview(label)
+    }
+
+}
+
+extension AchievementsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return bank.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AchievementCell", for: indexPath) as? AchievementCollectionViewCell else{
+            fatalError("Não foi possivel instanciar uma AchievementCollectionViewCell")
+        }
+        let achievement = bank.getAchievement(indexPath.row)
+        cell.imageView.image = achievement.getImage()
+        if achievement.isCompleted(){
+            
+            cell.frontView.isHidden = true
+        }
+        else{
+            cell.frontView.isHidden = false
+        }
+        
+        return cell
+    }
+    
+
+    public override func loadView() {
         self.view = UIView()
         view.backgroundColor = .myLightBlue
         let orangeTab = UIView(frame: CGRect(x: 0, y: 0, width: 1024, height: 106))
@@ -35,11 +108,57 @@ public extension AchievementsViewController{
         backButton.frame = CGRect(x: 30, y: 30, width: 54, height: 42)
         backButton.setImage(UIImage(imageLiteralResourceName: "back.png"), for: .normal)
         backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.headerReferenceSize = CGSize(width: self.view.frame.width, height: 100)
+        let achievementCollection = UICollectionView(frame: CGRect(x: 100, y: 106, width: self.width - 200, height: self.height - 106), collectionViewLayout: flowLayout)
         
+        achievementCollection.backgroundColor = .clear
+        achievementCollection.register(AchievementCollectionViewCell.self, forCellWithReuseIdentifier: "AchievementCell")
+
+        achievementCollection.register(SupView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerCell")
+        
+        achievementCollection.delegate = self
+        achievementCollection.dataSource = self
+        achievementCollection.reloadData()
         view.addSubview(orangeTab)
         orangeTab.addSubview(logoBackground)
         logoBackground.addSubview(label)
         orangeTab.addSubview(backButton)
+        view.addSubview(achievementCollection)
+        
+        
+    }
+    public func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    public func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerCell", for: indexPath) as? SupView else{
+                fatalError("Não foi possível gerar SupView")
+            }
+            header.frame = CGRect(x: 0, y: 0, width: collectionView.frame.width, height: 100)
+            
+            return header
+        default:
+            // 4
+            assert(false, "Invalid element type")
+        }
     }
     
     @objc func goBack(){
@@ -47,6 +166,8 @@ public extension AchievementsViewController{
         navigationController?.popViewController(animated: true)
 
     }
+    
+
 }
 
 let vc = MapViewController(screenType: .ipad, isPortrait: false)
---
+++ cocreate.playground/Sources/Achievement.swift
@@ -0,0 +1,56 @@
+import Foundation
+import UIKit
+
+public class Achievement{
+    private let image: UIImage
+    private let title: String
+    private let text: String
+    private var completed: Bool
+    public init(title: String, text: String, imageName: String) {
+        self.title = title
+        self.text = text
+        self.image = UIImage(imageLiteralResourceName: imageName)
+        self.completed = false
+    }
+    public func getText()->String{return self.text}
+    public func getTitle()->String{return self.title}
+    public func getImage()->UIImage{return self.image}
+    
+    public func complete(){
+        self.completed = true
+    }
+    public func isCompleted()->Bool{return self.completed}
+}
+
+public class AchievementBank{
+    var achievements: [Achievement] = []
+    public var count: Int{
+        get {achievements.count}
+    }
+    public init() {
+        achievements.append(Achievement(title: "a", text: "d", imageName: "mark"))
+        achievements.append(Achievement(title: "b", text: "e", imageName: "premio"))
+        achievements.append(Achievement(title: "c", text: "f", imageName: "mark"))
+        achievements.append(Achievement(title: "g", text: "k", imageName: "premio"))
+        achievements.append(Achievement(title: "h", text: "l", imageName: "premio"))
+        achievements.append(Achievement(title: "j", text: "m", imageName: "premio"))
+        achievements.append(Achievement(title: "n", text: "r", imageName: "mark"))
+        achievements.append(Achievement(title: "o", text: "s", imageName: "mark"))
+        achievements.append(Achievement(title: "p", text: "t", imageName: "premio"))
+        achievements.append(Achievement(title: "q", text: "u", imageName: "mark"))
+        achievements[0].complete()
+        achievements[1].complete()
+        achievements[2].complete()
+        achievements[3].complete()
+    }
+    public func getAchievement(_ index: Int) -> Achievement{
+        if index < achievements.count {
+            return achievements[index]
+        }
+        else{
+            print("Não existe conquista neste índice")
+            return Achievement(title: "Nulo", text: "Não tem nada aki", imageName: "icons-menu")
+        }
+    }
+
+}
